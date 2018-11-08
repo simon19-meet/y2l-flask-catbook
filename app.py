8e6ed01b6306f40496251a1ff0c310167e1eae2a
Simon Deeb <simon19-meet@meet.mit.edu>
2018-11-08 09:02:04 -0800

    finished lab 1

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-from database import get_all_cats
+from database import *
 
 app = Flask(__name__)
 
@@ -9,5 +9,10 @@ def catbook_home():
     cats = get_all_cats()
     return render_template("home.html", cats=cats)
 
+@app.route('/cats/<int:id>')
+def cat_1(id):
+    cat = get_cat_by_id(id)
+    return render_template("cat.html",cat=cat.name)
+    
 if __name__ == '__main__':
    app.run(debug = True)
